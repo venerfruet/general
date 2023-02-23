205275468c1f4fc673c44bac0bfc2f9c0e9b5d01
Vener Fruet da Silveira <105865020+venerfruet@users.noreply.github.com>
2023-02-23 09:29:00 -0300

    Add files via upload

---
+++ general-javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.com.senai.bauru.vener.general"}];updateSearchResults();
---
+++ general-javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"br.com.senai.bauru.vener.general","l":"General"}];updateSearchResults();
