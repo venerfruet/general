4f7174f6a44f186cf7462708363e6745e2bcd8be
Vener Fruet da Silveira <105865020+venerfruet@users.noreply.github.com>
2023-02-23 09:37:11 -0300

    Delete general-javadoc directory

--- general-javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.com.senai.bauru.vener.general"}];updateSearchResults();
--- general-javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"br.com.senai.bauru.vener.general","l":"General"}];updateSearchResults();
